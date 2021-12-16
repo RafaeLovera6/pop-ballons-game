0f6cc7b4285c5a263a838b1b00e573e71c981a74
RafaeLovera6 <rafaelovera6@gmail.com>
2021-12-16 02:21:47 +0000

    missing the final logic

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let balloons = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "purple",
+  "pink",
+  "black",
+  "white",
+  "blue",
+  "green",
+  "blue",
+  "blue",
+  "yellow",
+  "purple",
+  "black",
+  "white",
+  "blue",
+  "green",
+  "purple",
+  "red"
+];
+window.onload = render;
+
+function render() {
+  document.querySelector(".container").innerHTML = balloons
+    .map(elm => drawGrid(elm))
+    .join("");
+}
+function drawGrid() {
+  return `<div class="balloon">
+</div>`;
+}
